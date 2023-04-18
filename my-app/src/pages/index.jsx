b48ca62c741f4b6b42dcc2377bf9c82bd2f94405
Carlos Silva <silvaugusto222@gmail.com>
2023-04-18 02:16:55 -0300

    style aplication

--- my-app/src/pages/index.jsx
+++ my-app/src/pages/index.jsx
@@ -1,30 +1,52 @@
 import styles from '@/styles/Home.module.css'
+
 import { useEffect, useState } from 'react'
 
 
 export default function Home() {
 
   const [posts, setPosts] = useState([])
+  const [HeroPost, setHeroPost] = useState(null)
 
   useEffect(() => {
     fetch(`https://news-api.lublot.dev/api/posts`)
       .then(response => response.json())
-      .then(data => setPosts(data))
-      .catch(error => console.error(error));
+      .then(data => {
+        setPosts(data.slice(0, 10))
+        setHeroPost(data[Math.floor(Math.random() * data.length)])
+      })
+      .catch(error => console.error(error))
   }, [])
 
+  console.log(posts[1])
 
   return (
     <div>
+
+        {HeroPost && (
+          <hero className={styles.hero}>
+            <img src={HeroPost.coverImage} alt="" />
+            <h2>{HeroPost.title}</h2>
+            <p>{HeroPost.description}</p>
+            <p>Escrito por: {HeroPost.author}</p>
+            <p>{HeroPost.content.substring(0, 255)}
+            </p>
+          </hero>
+        )}
+
+        
       <main className={styles.main}>
-        <ol>
+        <h1>Ultimas Noticias</h1>
+        <ul className={styles.listNews}>
           {posts.map(post => (
-            <li key={post.id}>
+            <li key={post.id} className={styles.cardNews}>
+              <img src={post.coverImage} alt="" />
               <h3>{post.title}</h3>
               <p>{post.description}</p>
+              <p>Escrito por: {post.author}</p>
             </li>
           ))}
-        </ol>
+        </ul>
       </main>
     </div>
   )
